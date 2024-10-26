f359330e2ce578c4cc748d26682451e7a31f3a10
David-Salihu <davidsalihu19@gmail.com>
2024-10-26 06:49:51 +0100

    Update code to use redpanda cloud

--- server/redpanda/admin.ts
+++ server/redpanda/admin.ts
@@ -11,16 +11,9 @@ export async function createTopic( topics: string[], partitions?: number, replic
       {
         topic: topic,
         numPartitions: partitions ? partitions : 1,
-        replicationFactor: replicas ? replicas : 1,
+        replicationFactor: replicas ? replicas : 3,
       }
     ))
   });
   await admin.disconnect();
-}
-
-export async function topicExists(topic: string) {
-  await admin.connect();
-  const existingTopics = await admin.listTopics();
-  await admin.disconnect();
-  return existingTopics.includes(topic);
 }
--- server/redpanda/redpanda_config.ts
+++ server/redpanda/redpanda_config.ts
@@ -5,5 +5,12 @@ env.config();
 
 const config: KafkaConfig = {
   brokers: JSON.parse(process.env.KAFKA_BROKERS || '["localhost:9092"]'),
+  ssl: {
+    },
+  sasl: {
+    mechanism: "scram-sha-256",
+    username: process.env.KAFKA_USERNAME || 'your_username', 
+    password: process.env.KAFKA_PASSWORD || 'your_password', 
+  }
 };
 export const redpanda = new Kafka(config);
