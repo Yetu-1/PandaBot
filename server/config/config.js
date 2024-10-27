3396e0c76a82abf315cc05bc86488b937fef27bd
Joshua Owolabi <joshuaoluwasegun99@gmail.com>
2024-10-27 08:30:50 +0100

    fix redpanda connection issue

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,10 @@ const config = {
   production: {
     dialect: "postgres",
     dialectOptions: {
-      ssl: true,
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false, // This line will fix new error
+      },
     },
     use_env_variable: "DATABASE_URL",
   },
