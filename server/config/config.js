7da4af1d9d7a3b63ca393d6a2120d7b64b5da9ff
Joshua Owolabi <joshuaoluwasegun99@gmail.com>
2024-10-27 08:06:44 +0100

    fix redpanda connection issue

--- server/config/config.js
+++ server/config/config.js
@@ -1,29 +1,28 @@
-import env from "dotenv"
+import env from "dotenv";
 env.config();
 
 const config = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
   },
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
+  production: {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true,
+    },
     use_env_variable: "DATABASE_URL",
-  }
-}
+  },
+};
 
 export default config;
